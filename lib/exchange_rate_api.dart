81e3b9b656171b4eae8b28c04c4be664200a9050
Hannes Dorfmann <sockeqwe@users.noreply.github.com>
2024-01-25 11:41:00 +0100

    Update exchange_rate_api.dart

--- lib/exchange_rate_api.dart
+++ lib/exchange_rate_api.dart
@@ -4,8 +4,18 @@ import 'package:currency_converter/exchange_rate.dart';
 
 class ExchangeRateApi {
   List<ExchangeRate> getExchangeRage() => _loadExchangeRates();
+
+  List<ExchangeRate> _loadExchangeRates() => [
+        ExchangeRate("GBP", 0.79),
+        ExchangeRate("EUR", 0.92),
+        ExchangeRate("USD", 1.0),
+      ];
+
+  /*
+  // Just ignore the code below.
+  // if we have time left, we will implement this one below.
   Future<List<ExchangeRate>> loadExchangeRate() =>
-      Future.delayed(const Duration(seconds: 1), () => _returnListOrError());
+    Future.delayed(const Duration(seconds: 1), () => _returnListOrError());
 
   Future<List<ExchangeRate>> _returnListOrError() {
     Random r = Random();
@@ -16,10 +26,5 @@ class ExchangeRateApi {
       return Future(() => throw Exception("Fake Exception"));
     }
   }
-
-  List<ExchangeRate> _loadExchangeRates() => [
-        ExchangeRate("GBP", 0.79),
-        ExchangeRate("EUR", 0.92),
-        ExchangeRate("USD", 1.0),
-      ];
+  */
 }
